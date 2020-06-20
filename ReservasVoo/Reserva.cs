d4506b004c7aa52bb02ccc027d907f852759a162
Elder <eldercnw@gmail.com>
2020-06-20 14:14:04 -0300

    classes adicionadas

---
+++ ReservasVoo/Reserva.cs
@@ -0,0 +1,28 @@
+﻿using ReservasVoo.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservasVoo
+{
+    class Reserva
+    {
+        public Cliente Cliente { get; set; }
+        public Voo Voo { get; set; }
+        public Compra Compra { get; set; }
+        private static int TotalReservas = 0;
+
+        public static int EnviarTotalReservas()
+        {
+            return TotalReservas;
+        }
+        
+        public static void CancelarReserva()
+        {
+
+        }
+    }
+}
---
+++ ReservasVoo/Voos/VooEmpresarial.cs
@@ -0,0 +1,14 @@
+﻿using ReservasVoo.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservasVoo.Voos
+{
+    class VooEmpresarial
+    {
+        public PessoaJuridica Empresa { get; set; }
+    }
+}
